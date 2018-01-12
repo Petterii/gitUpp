c2c2cab3355b5273cf7b45a04f8f733a1b711521
Petteri <Buggis123buggis@gmail.com>
2018-01-12 14:06:32 +0100

    Initial Commit

---
+++ GitTraining/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTraining
+//
+//  Created by lösen är 0000 on 2018-01-12.
+//  Copyright © 2018 Petteri Tuononen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
